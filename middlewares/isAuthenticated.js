7cd869e877c64a4367543b60fdf5336e8b273768
sberlinches <sergioberlinches@gmail.com>
2017-10-29 16:25:28 -0700

    Changed HTTP status and message response

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -13,6 +13,6 @@ module.exports = function (req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.status(403).send('Forbidden');
+        res.status(401).send('Unauthorized');
     }
 };
