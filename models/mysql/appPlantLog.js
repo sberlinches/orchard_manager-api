8468c38ada9a2801bfff450b2079d47bebc4eb28
sberlinches <sergioberlinches@gmail.com>
2017-11-08 16:51:22 -0800

    Typo: recomended > recommended

--- models/mysql/appPlantLog.js
+++ models/mysql/appPlantLog.js
@@ -104,8 +104,8 @@ module.exports = function(sequelize, Sequelize) {
         var sql = "SELECT current.isGerminated, current.isMature, current.createdAt, ";
             sql += "current.airTemp AS 'current.airTemp', current.soilTemp AS 'current.soilTemp', current.light AS 'current.light', current.airHum AS 'current.airHum', current.soilMoist AS 'current.soilMoist', current.ph AS 'current.ph', ";
             sql += "average.airTemp AS 'average.airTemp', average.soilTemp AS 'average.soilTemp', average.light AS 'average.light', average.airHum AS 'average.airHum', average.soilMoist AS 'average.soilMoist', average.ph AS 'average.ph', ";
-            sql += "recomended.airTempMin AS 'recomendedMin.airTemp', recomended.soilTempMin AS 'recomendedMin.soilTemp', recomended.lightMin  AS 'recomendedMin.light', recomended.airHumMin AS 'recomendedMin.airHum', recomended.soilMoistMin AS 'recomendedMin.soilMoist', recomended.phMin AS 'recomendedMin.ph', ";
-            sql += "recomended.airTempMax AS 'recomendedMax.airTemp', recomended.soilTempMax AS 'recomendedMax.soilTemp', recomended.lightMax AS 'recomendedMax.light', recomended.airHumMax AS 'recomendedMax.airHum', recomended.soilMoistMax AS 'recomendedMax.soilMoist', recomended.phMax AS 'recomendedMax.ph' ";
+            sql += "recommended.airTempMin AS 'recommendedMin.airTemp', recommended.soilTempMin AS 'recommendedMin.soilTemp', recommended.lightMin  AS 'recommendedMin.light', recommended.airHumMin AS 'recommendedMin.airHum', recommended.soilMoistMin AS 'recommendedMin.soilMoist', recommended.phMin AS 'recommendedMin.ph', ";
+            sql += "recommended.airTempMax AS 'recommendedMax.airTemp', recommended.soilTempMax AS 'recommendedMax.soilTemp', recommended.lightMax AS 'recommendedMax.light', recommended.airHumMax AS 'recommendedMax.airHum', recommended.soilMoistMax AS 'recommendedMax.soilMoist', recommended.phMax AS 'recommendedMax.ph' ";
             sql += "FROM `app-plantLog` AS current ";
             sql += "INNER JOIN ( ";
             sql += "SELECT id, ROUND(AVG(airTemp),2) AS airTemp, ROUND(AVG(soilTemp),2) AS soilTemp, ROUND(AVG(light)) AS light, ROUND(AVG(airHum),2) AS airHum, ROUND(AVG(soilMoist),2) AS soilMoist, ROUND(AVG(ph),2) AS ph ";
@@ -119,7 +119,7 @@ module.exports = function(sequelize, Sequelize) {
             sql += "SELECT zones_varieties.id, airTempMin, airTempMax, soilTempMin, soilTempMax, lightMin, lightMax, airHumMin, airHumMax, soilMoistMin, soilMoistMax, phMin, phMax ";
             sql += "FROM `app-zones_varieties` AS zones_varieties ";
             sql += "INNER JOIN `app-variety` AS variety ON zones_varieties.varietyId = variety.id ";
-            sql += ") AS recomended ON recomended.id = current.id ";
+            sql += ") AS recommended ON recommended.id = current.id ";
             sql += "WHERE current.id = :plantLogId;";
 
         return AppPlantLog.sequelize.query(sql, {
